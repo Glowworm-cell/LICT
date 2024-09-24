ce810f0459f9b1143398b370f83cc88a13c01dd6
Glowworm-cell <yewj27@mail2.sysu.edu.cn>
2024-09-24 19:40:09 +0800

    Add files via upload

---
+++ R/MarkerPrompt.R
@@ -0,0 +1,31 @@
+#' @import reticulate
+#' @import dplyr
+#' @import stringr
+#' @export
+#'
+MarkerPrompt = function(result, model, species){
+  mg <- character()
+  cluster = numeric()
+  row_number <- 1  # 初始化行号
+  res1 = result
+  for (i in 1:nrow(res1)) {
+    n = res1[i,]# 获取当前行数据
+    # 处理并确保以字符形式处理单元格内容，排除NA值
+    ct <- as.character(n[[model]])
+    ct <- na.omit(ct)  # 排除NA值
+    if (length(ct) > 0) {  # 只有在ct不为空的情况下才进行下一步
+      ct_text <- paste(ct, collapse=", ")
+      mg <- c(mg, paste("row", row_number, ":", ct_text))
+    }
+    cluster = c(cluster,i)
+    row_number <- row_number + 1  # 增加行号
+  }
+
+  # 将mg的各行合并成一个长字符串，每行之间用换行符分隔
+  final_text <- paste(mg, collapse="\n")
+
+  specie = species
+  # 生成最终输出
+  output_text <- paste('Provide key marker genes for the following ', specie, ' cell types, with 15 key marker genes per cell type. Provide only the abbreviated gene names of key marker genes, full names are not required:\n', final_text, '\nThe format of the final response should be:\n\n1: gene1, gene2, gene3\n2: gene1, gene2, gene3\nN: gene1, gene2, gene3\n\n...where N represents the row number and gene1, gene2, gene3 represent key marker genes.', sep="")
+  print(output_text)
+}
